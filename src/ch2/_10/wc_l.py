fde3743dc5e4e6956519154c79c6aedbc100b9e4
kamiazya <yuki@kamiazya.tech>
2020-02-08 13:07:52 +0900

    add work 10 #3

---
+++ src/ch2/_10/wc_l.py
@@ -0,0 +1,24 @@
+import sys
+from pathlib import Path
+from select import select
+from contextlib import ExitStack
+
+
+def is_pipe():
+    if select([sys.stdin, ], [], [], 0.0)[0]:
+        return True
+    return False
+
+
+def wc_l(*filepath: str) -> int:
+    c = 0
+    with ExitStack() as stack:
+        for fp in filepath:
+            f = stack.enter_context(open(fp, 'r'))
+            c += len(f.readlines())
+    return c
+
+
+if __name__ == "__main__":
+    filename = str((Path(__file__) / "../../hightemp.txt").resolve())
+    print(wc_l(filename))
---
+++ tests/ch2/_10/test_wc_l.py
@@ -0,0 +1,19 @@
+import unittest
+from pathlib import Path
+import subprocess
+from src.ch2._10.wc_l import wc_l
+
+
+class TestWcL(unittest.TestCase):
+
+    def test_wc_l(self):
+        filename = str(
+            (Path(__file__) / "../../../../src/ch2/hightemp.txt").resolve())
+        result = wc_l(filename)
+        command = f"cat {filename} | wc -l | awk " + "'{print $1}'"
+        check_wc = subprocess.check_output(command, shell=True)
+        self.assertEqual(result, int(check_wc))
+
+
+if __name__ == "__main__":
+    unittest.main()
