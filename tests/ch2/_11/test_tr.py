f21bbb8ab1575771d33ec9bf5af0b2c1ddd2d7c9
kamiazya <yuki@kamiazya.tech>
2020-02-09 16:11:53 +0900

    add work11

---
+++ tests/ch2/_11/test_tr.py
@@ -0,0 +1,19 @@
+import unittest
+from pathlib import Path
+import subprocess
+from src.ch2._11.tr import tr
+
+
+class TestTr(unittest.TestCase):
+
+    def test_tr(self):
+        filename = str(
+            (Path(__file__) / "../../../../src/ch2/hightemp.txt").resolve())
+        result = tr("\t", " ", filename)
+        command = f"cat {filename} | tr '\\t' ' '"
+        check_wc = subprocess.check_output(command, shell=True)
+        self.assertEqual(result, check_wc.decode())
+
+
+if __name__ == "__main__":
+    unittest.main()
