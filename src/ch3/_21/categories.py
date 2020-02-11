4dca06ff2ce806be830bc168fa205862edbe7221
kamiazya <yuki@kamiazya.tech>
2020-02-11 23:13:42 +0900

    add work21 #14

---
+++ src/ch3/_21/categories.py
@@ -0,0 +1,29 @@
+import re
+from os import linesep
+from pathlib import Path
+from typing import List
+
+from src.ch3._20.ndjson import parse
+
+_categories_regex = re.compile(r"\[\[Category:.+\]\]")
+
+
+def get_category_lines(article: str) -> List[str]:
+    return _categories_regex.findall(article)
+
+
+def main(filepath: str):
+    with open(filepath, "r") as file:
+        for data in parse(file):
+            title = data["title"]
+            article = data["text"]
+            if type(article) == str and type(title) == str:
+                categories = get_category_lines(article)
+
+                print(title, *categories, sep=linesep)
+
+
+if __name__ == "__main__":
+    workdir = Path(__file__).parent
+    filepath = str((workdir / "../jawiki-country.json").resolve())
+    main(filepath)
