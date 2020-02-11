18d5f05b1677ed0760d50f94e9a20ccc88d589b0
kamiazya <yuki@kamiazya.tech>
2020-02-12 00:50:27 +0900

    fix codeing style

--- tests/ch3/_24/test_get_media.py
+++ tests/ch3/_24/test_get_media.py
@@ -19,7 +19,8 @@ from src.ch3._24.get_media import get_media
                 "[[ファイル:All Gizah Pyramids.jpg|thumb|300px|right|[[ギーザ|ギザ]]の[[三大ピラミッド]]。]]\n"
                 "== め ==\n"
                 "[[Category:カフカス]]\n"
-                "[[ファイル:Egyptiska hieroglyfer, Nordisk familjebok.png|thumb|260px|right|[[ヒエログリフ]]。]]\n"
+                "[[ファイル:Egyptiska hieroglyfer, Nordisk familjebok.png|"
+                "thumb|260px|right|[[ヒエログリフ]]。]]\n"
                 "[[Category:オセット]]\n"
             ),
             ["All Gizah Pyramids.jpg", "Egyptiska hieroglyfer, Nordisk familjebok.png"],
@@ -30,7 +31,8 @@ from src.ch3._24.get_media import get_media
                 "[[Category:日本|*]]\n"
                 "== うえ ==\n"
                 "[[Category:島国]]\n"
-                "[[ファイル:Hosni Mubarak ritratto.jpg|thumb|left|[[アラブの春]]で失脚するまで30年以上に渡り長期政権を維持した[[ホスニー・ムバーラク]]]]\n"
+                "[[ファイル:Hosni Mubarak ritratto.jpg|thumb|left|[[アラブの春]]"
+                "で失脚するまで30年以上に渡り長期政権を維持した[[ホスニー・ムバーラク]]]]\n"
                 "お\n"
                 "[[Category:君主国]]\n"
                 "== かき く ==\n"
